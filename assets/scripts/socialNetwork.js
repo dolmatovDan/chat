9c5986d9c6cc01fcf9e55ac1fe649367b26d20b1
Polina Shneider <beatrixlinkoln@gmail.com>
2020-12-28 22:34:53 +0300

    Chat fixes

--- assets/scripts/socialNetwork.js
+++ assets/scripts/socialNetwork.js
@@ -1,36 +1,40 @@
 class SocialNetwork {
-	constructor() {
-		this.groups = document.getElementById('groups');
-		this.addListeners();
-		this.dialogues = {};
-		//this.chat = new Chat({id: 0, messages: this.dialogues[0] || []});
-	}
+    constructor() {
+        this.groups = document.getElementById('groups');
+        this.addListeners();
+        this.dialogues = {};
+        this.chat = new Chat({id: 0, messages: this.dialogues[0] || []});
+    }
 
-	addListeners() {
-		this.groups.addEventListener('click', (e) => {
-			const targetElem = e.target;
-			const parent = targetElem.closest('.group');
-			if(!parent) {
-				return;
-			}
-			const dialogueId = parent.getAttribute('data-id');
-			
-			
-			this.toggleChat(dialogueId);
-		})
-	}
+    addListeners() {
+        this.groups.addEventListener('click', (e) => {
+            const targetElem = e.target;
+            const parent = targetElem.closest('.group');
+            if (!parent) {
+                return;
+            }
+            const dialogueId = parent.getAttribute('data-id');
+            
+            // CSS-магия активного диалога (лучше обработчики одного элемента держать вместе)
+            const arrElem = document.querySelectorAll('.group');
+            for (let i = 0; i < arrElem.length; i++) {
+                arrElem[i].classList.remove('group--click');
+            }
+            if (parent) {
+                parent.classList.add('group--click');
+            }
+            this.toggleChat(dialogueId);
+        })
+    }
 
-	toggleChat(dialogueId) {
-		if(this.chat){
-				this.dialogues[this.chat.id] = [...this.chat.messagesQueue];
-				this.chat.destroy();
-				console.log(this.chat.id);
-				console.log(this.chat.messagesQueue);
-				console.log(this.dialogues);
-			}
-			
-			this.chat = new Chat({id: dialogueId, messages: this.dialogues[dialogueId]});
-		}	
+    toggleChat(dialogueId) {
+        if (this.chat.id) {
+            this.dialogues[this.chat.id] = this.chat.messagesQueue || [];
+            this.chat.destroy();
+        }
+
+        this.chat.init({id: dialogueId, messages: this.dialogues[dialogueId]});
+    }
 
 }
 
